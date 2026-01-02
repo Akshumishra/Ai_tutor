3754d58b5d774a59dab003c650d546bed6c846e6
akshu-mishra <akshita@gkmit.co>
2026-01-02 14:28:17 +0530

    feat: add database engine and session configuration

---
+++ src/backend/db/database.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+from src.backend.config import Config
+
+engine = create_engine(Config.DB_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
