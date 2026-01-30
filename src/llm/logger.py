23fa3f3cf3d2c158138356661287f5913e57a1d4
akshu-mishra <akshita@gkmit.co>
2026-01-30 13:40:46 +0530

    chore: clean up comment out lines in code:

--- src/llm/logger.py
+++ src/llm/logger.py
@@ -1,12 +1,3 @@
-# import logging
-# from src.llm.config import LLMConfig
-
-
-# def setup_logging():
-#     logging.basicConfig(
-#         level=LLMConfig.LOG_LEVEL,
-#         format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
-#     )
 import logging
 from pathlib import Path
 from src.llm.config import LLMConfig
